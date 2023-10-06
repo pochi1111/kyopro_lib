1e369a3c381de90b45b40a5268f6544958210220
pochi1111 <kuroinusanshinji@gmail.com>
2023-10-06 21:17:00 +0900

    temp追加

---
+++ template/template.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+constexpr int inf = 1 << 30;
+constexpr ll INF = 1LL << 60;
+#define rep(i, n) for (int (i) = 0; (i) < (int)(n); (i)++)
