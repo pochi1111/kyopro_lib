cbd89e5223144565d677ef0206fa44571df6e158
pochi1111 <kuroinusanshinji@gmail.com>
2023-11-22 21:01:53 +0900

    ちょっと変更

--- example.test.cpp
+++ example.test.cpp
@@ -1,22 +1,16 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define ALL(x) std::begin(x), std::end(x)
-using namespace std;
-
+#include "library/template/template.hpp"
 int main() {
     int n; cin >> n;
     vector<int> s(n);
-    REP (i, n) {
+    rep (i, n) {
         cin >> s[i];
     }
     int q; cin >> q;
     int cnt = 0;
     while (q --) {
         int t_i; cin >> t_i;
-        cnt += binary_search(ALL(s), t_i);
+        cnt += binary_search(s.begin(),s.end(), t_i);
     }
     cout << cnt << endl;
     return 0;
