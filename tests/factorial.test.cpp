584708b99fed20c0331dd5a0b7db8f347f2c2aa2
kuroinusan <kuroinusanshinji@gmail.com>
2023-11-26 17:39:26 +0900

    階乗のテスト追加

---
+++ tests/factorial.test.cpp
@@ -0,0 +1,7 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0019"
+#include "template/template.hpp"
+#include "math/factorial.hpp"
+int main(){
+    int N; cin >> N;
+    cout << factorial(N) << endl;
+}
