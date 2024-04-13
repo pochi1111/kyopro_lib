12aca19f501fd5a6d80da71bd8e9590ea22f64b3
kuroinusan <kuroinusanshinji@gmail.com>
2024-04-13 16:15:16 +0900

    aclを初期から読み込み

--- template/template.hpp
+++ template/template.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 #define all(x) (x).begin(), (x).end()
 template <typename T, typename U>
